c3475f5db6ed18cbf88ae40a26a84de065a79423
yusuf ridwan <horiyorrmi72@gmal.com>
2021-02-17 08:55:58 +0100

    findingTheLargestNumbersInArrays

---
+++ findingLargestNumberInAnArray.js
@@ -0,0 +1,21 @@
+function largestOfFour(arr) {
+  var output = [];
+  for (var i = 0; i < arr.length; i++) {
+    var maxNumber = arr[i][0];
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > maxNumber) {
+        maxNumber = arr[i][j];
+      }
+    }
+    output[i] = maxNumber;
+  }
+  return output;
+}
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
