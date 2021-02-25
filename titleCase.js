01c38ee942682d242586b10fd768e8ec20e8b285
yusuf ridwan <horiyorrmi72@gmal.com>
2021-02-25 17:02:11 +0100

    Update titleCase.js

--- titleCase.js
+++ titleCase.js
@@ -2,19 +2,17 @@
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
-    let arr =  str.toLowerCase().split(' ');
-    for(var i = 0; i < arr.length; i++){
-    //   let firstChar = arr[i][0].toUpperCase();
-      let replaceRegex = /(^|\s)\S/g;
-  
-      return str.replace(replaceRegex,subStr => subStr.toUpperCase());
-    }
-  }
-  
-  console.log(titleCase("I'm a little tea pot"));
+  let newString = str.toLowerCase();
+    let replaceRegex = /(^|\s)\S/g;
 
+    return newString.replace(replaceRegex,subStr => subStr.toUpperCase());
+  
+}
+
+console.log(titleCase("I'm a little tea pot"));
+  
 //   Code Explanation
-// The solution works by first lowercasing all the characters in the string and then only uppercasing the first character of each word.
+// The solution works by first  changing to lowercase all the characters in the string and then only change the first character of each word to uppercase.
 
 // Lowercase the whole string using str.toLowerCase().
 // Replace every word’ first character to uppercase using .replace.
