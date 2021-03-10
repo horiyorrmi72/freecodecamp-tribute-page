9a7c1303ed496159c9be54b3942aab3e16f0decd
yusuf ridwan <horiyorrmi72@gmal.com>
2021-03-10 15:52:42 +0100

    Update test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+function cardChecker(_cardNumber, _cardName) {
+  card[0] = {
+    type: "masterCard",
+    length: "16",
+    firstChar: "51,52,53,54,55",
+  };
+  card[1] = {
+    type: "visa",
+    length: "14",
+    firstChar: "4",
+  };
+  card[2] = {
+    type: "verve",
+    length: "13",
+    firstChar: "2",
+  };
+
+  for(var i = 0; i < card.length; i++){
+      
+  }
+}
